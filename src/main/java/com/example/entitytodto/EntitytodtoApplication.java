db959067d78ed3be7b345547048b45bcd91ec1a9
nam <namgd69@gmail.com>
2021-11-09 09:35:30 +0700

    etod project

---
+++ src/main/java/com/example/entitytodto/EntitytodtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.entitytodto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitytodtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntitytodtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/entitytodto/EntitytodtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entitytodto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntitytodtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
